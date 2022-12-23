7f9b74877fe735c3d7d725dd6e44ddf632ea73ca
DasLixou <einlixou@gmail.com>
2022-12-23 16:58:33 +0100

    Release `0.1.4`

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,13 @@
+//! bevy_ecs_markers is a helper for bevy_ecs which adds the support to mark entites
+//!
+//! # Features
+//! - **proc** *(default)* &mdash; re-exports procedual macros from `bevy_ecs_markers_macros`
 mod entity_marker;
 mod marker_data;
 pub mod params;
 
 pub use entity_marker::*;
 pub use marker_data::*;
+
+#[cfg(feature = "proc")]
+pub use bevy_ecs_markers_macros::EntityMarker;
