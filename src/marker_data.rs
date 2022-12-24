13194fd1f1f2123543562e4b2c00577b79a27f24
DasLixou <einlixou@gmail.com>
2022-12-24 14:19:41 +0100

    MarkerData traits docs

--- src/marker_data.rs
+++ src/marker_data.rs
@@ -2,11 +2,13 @@ use bevy_ecs::entity::Entity;
 
 use crate::entity_marker::EntityMarker;
 
+/// This trait is used for MarkerData when it only holds one [`Entity`]
 pub trait SingleMarkerData<M: EntityMarker> {
     fn get(&self) -> &Entity;
     fn get_mut(&mut self) -> &mut Entity;
 }
 
+/// This trait is used for MarkerData when it holds multiple of [`Entity`] and can be retrieved by a key
 pub trait ValueMarkerData<M: EntityMarker> {
     fn value(&self, key: M) -> &Entity;
     fn value_mut(&mut self, key: M) -> &mut Entity;
