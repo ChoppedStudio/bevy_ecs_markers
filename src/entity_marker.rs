63fc58d97705a4f5b84b6d4b74dfbf3366b91d9c
DasLixou <einlixou@gmail.com>
2022-12-24 14:16:47 +0100

    MarkerData docs

--- src/entity_marker.rs
+++ src/entity_marker.rs
@@ -1,5 +1,9 @@
 use bevy_ecs::system::Resource;
 
+/// Defines which MarkerData to use for this EntityMarker
+///
+/// ## Hint
+/// `#[derive(EntityMarker)]` automaticly creates and links a matching MarkerData type for the type to which the derive is applied to.
 pub trait EntityMarker: Sync + Send {
     type MarkerData: Resource + Default;
 }
